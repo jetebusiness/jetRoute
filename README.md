e4cbd1f4c94ed5126bc946452283acddd3b69e89
Heitor Ramon <heitor.ramon@gmail.com>
2017-08-08 17:00:01 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# jetRoute
-A simpe route manager for a fake SPA webpage with single loaded script.
+# jetRoute - Simple Route Manager
+
+> A simple jQuery route manager for fake SPA scripts, to be implemented in a fake SPA scenarios.
+
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.githubusercontent.com/bloo_df/jquery-jetroute/master/dist/jquery.jetroute.min.js
+[max]: https://raw.githubusercontent.com/bloo_df/jquery-jetroute/master/dist/jquery.jetroute.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jetroute.min.js"></script>
+<script>
+  jQuery(function ($) {
+    $.awesome(); // "awesome"
+  });
+</script>
+```
+
+
+## License
+
+Apache 2.0 © Heitor Ramon Ribeiro
---
+++ src/jetroute.js
@@ -0,0 +1,97 @@
+/**
+ * jetRoute
+ * Author: Heitor Ramon Ribeiro
+ * Email: heitor.ramon@gmail.com
+ * Site: heitorramon.com
+ */
+;(function ($, window, document, undefined) {
+    "use strict";
+    /**
+     * $.jetRoute Plugin
+     * @param route (Initial Route)
+     * @param callback (Execute if Route is the current)
+     * @param options (Route Options)
+     * @returns {*}
+     */
+    $.jetRoute = function (route, callback, options) {
+        let settings = $.extend({}, $.jetRoute.settings, options);
+        const routeManager = {
+            init(){
+                this.currentURL   = this.getUrlArray(this.getCurrentUrl(settings.routeType));
+                this.currentRoute = this.getUrlArray(this.getRoute(route));
+                document.routeParams = {
+                    route: this.getRoute(route)
+                };
+                if (this.checkRoute()) {
+                    callback();
+                }
+            },
+            getCurrentUrl(type){
+                if (type === "window") {
+                    return window.location.pathname;
+                }
+                if (type === "cookie") {
+                    let getCookie = (name) => {
+                        let cookie = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+                        return cookie ? cookie.pop() : '';
+                    };
+                    return getCookie(settings.cookieName);
+                }
+                if (typeof type === "function") {
+                    return type();
+                }
+            },
+            /**
+             * Return the URI of the called initial route.
+             * @param route
+             * @returns {*}
+             */
+            getRoute(route){
+                for (let key in settings.routes) {
+                    if (key === route) {
+                        return settings.routes[key];
+                    }
+                }
+            },
+            checkRoute(){
+                return this.compareUrlRoute(this.currentURL, this.currentRoute);
+            },
+            /**
+             * Route Compare and Dynamic Route Atributes
+             * @param currentURL
+             * @param currentRoute
+             * @returns {boolean}
+             */
+            compareUrlRoute(currentURL, currentRoute){
+                const regex     = /\{(.*)\}/i;
+                let arraySize   = currentRoute.length,
+                      testValue = false;
+                for (let i = 0; i < arraySize; i++) {
+                    let dynamic = regex.exec(currentRoute[i]);
+                    if (dynamic) {
+                        document.routeParams[dynamic[1]] = currentURL[i];
+                    } else {
+                        if (currentRoute[i] === currentURL[i]) {
+                            testValue = true;
+                        }
+                    }
+                }
+
+                return testValue;
+            },
+            getUrlArray(url){
+                let _url = url.split("/");
+                _url.shift();
+                return _url;
+            }
+        };
+
+        return routeManager.init();
+    };
+    $.jetRoute.settings = {
+        routes: {},
+        routeType: "window",
+        cookieName: ""
+    };
+
+})(jQuery, window, document);
